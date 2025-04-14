bb752ba88a86cfb04d6c5d2cfb9fde77cf087267
Minimert989 <ryuminwoo47298@gmail.com>
2025-04-15 01:35:19 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.static('public'));
 app.use('/auth', authRoutes);
 app.use('/posts', postRoutes);
 
-app.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
