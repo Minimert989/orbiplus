e29e2746b2a0b93b29cbd574fe45e53ae488ff18
Minimert989 <ryuminwoo47298@gmail.com>
2025-04-15 16:53:11 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ const postRoutes = require('./routes/posts');
 const commentRoutes = require('./routes/comments');
 const likeRoutes = require('./routes/likes');
 const adminRoutes = require('./routes/admin');  // ✅ 추가
-const backupRoutes = require('./routes/backup');
-const restoreRoutes = require('./routes/restore');
-app.use('/restore', restoreRoutes);
-app.use('/backup', backupRoutes);
+
 
 app.use(express.json());
 app.use(express.static('public'));
